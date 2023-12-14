6ee663ec36f933143ef11d7fa1b9294521c7d75b
Dello96 <enadios03@gmail.com>
2023-12-14 19:12:53 +0900

    first setting

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+      <MainBox></MainBox>
+    </>
+  );
+}
+
+const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default App;
